6be3916e4d416c08a51917e06f4fa76195903028
Joe <44414744+joebryant8@users.noreply.github.com>
2018-10-25 16:48:57 -0400

    Update Project 1.R

--- Project 1.R
+++ Project 1.R
@@ -2,3 +2,8 @@ this.dir <- dirname(parent.frame(2)$ofile)
 setwd(this.dir)
 legaladv = read.table('LEGALADV.txt')
 streetvn = read.table('STREETVN.txt')
+
+#Question 1
+#Part 1
+plot(legal$NewPI,legal$AdvExp6)
+plot(legal$NewWC,legal$AdvExp6)
